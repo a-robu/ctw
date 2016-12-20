c45530ac4146ef2ff6ce135949f9239f16c3b9b9
Andrei Robu <andrei.rocks.1992@gmail.com>
2016-12-20 17:08:00 +0000

    remove standard because it's flamewary

--- ctw.js
+++ ctw.js
@@ -1,19 +1,19 @@
 const memoizee = require('memoizee')
 
-let kt = memoizee(function (a, b) {
-  if (a === 0 && b === 0) {
-    return 1
-  }
-  if (a === 0) {
+const kt = memoizee(function (a, b) {
+    if (a == 0 && b == 0) {
+        return 1
+    }
+    if (a == 0) {
         // On wikipedia, we have:
         // P(m, n + 1) = P(m, n) * (n + 1/2) / (m + n + 1).
         // We set m = 0 and rewrite with b = n + 1, n = b - 1.
-    return kt(0, b - 1) * (b - 1 / 2) / b
-  }
-    // On wikipedia we have:
-    // P(m + 1, n) = P(m, n) * (m + 1/2) / (m + n + 1).
-    // We rewrite rewrite with a = m + 1, m = a - 1 and n = b.
-  return kt(a - 1, b) * (a - 1 / 2) / (a + b)
+        return kt(0, b - 1) * (b - 1 / 2) / b
+    }
+      // On wikipedia we have:
+      // P(m + 1, n) = P(m, n) * (m + 1/2) / (m + n + 1).
+      // We rewrite rewrite with a = m + 1, m = a - 1 and n = b.
+      return kt(a - 1, b) * (a - 1 / 2) / (a + b)
 })
 // TODO A memoized recursive function might not be the best
 // way to compute these values. The cache could grow very
--- ctw_spec.js
+++ ctw_spec.js
@@ -2,8 +2,8 @@ const expect = require('chai').expect
 const ctw = require('./ctw')
 
 describe('kt', () => {
-  it('should match an example', () => {
-        // A table of examples is in the doc directory.
-    expect(ctw.kt(4, 2)).to.equal(7 / 1024)
-  })
+    it('should match an example', () => {
+    // A table of examples is in the doc directory.
+        expect(ctw.kt(4, 2)).to.equal(7 / 1024)
+    })
 })
