bb0d63ce2b15d157b5ce7efd43c97057f6a835ce
Andrei Robu <andrei.rocks.1992@gmail.com>
2017-01-03 22:34:05 +0000

    organise unit tests a bit

--- ctw_spec.js
+++ ctw_spec.js
@@ -22,12 +22,50 @@ describe('kt', () => {
     })
 })
 
-describe('increment', () => {
-    it('works twice so 1 + 1 = 2', () => {
-        const empty = new ctw.Tree(3)
-        const first = empty.increment('010', '0')
-        const second = first.increment('010', '0')
-        expect(second.count('010', '0')).to.equal(2)
+describe('Tree', () => {
+    describe('increment', () => {
+        it('works twice so 1 + 1 = 2', () => {
+            const empty = new ctw.Tree(3)
+            const first = empty.increment('010', '0')
+            const second = first.increment('010', '0')
+            expect(second.count('010', '0')).to.equal(2)
+        })
+    })
+
+    describe('_elementary_count', () => {
+        it('returns zeroes for missing contexts', () => {
+            const tree = new ctw.Tree(3)
+            expect(tree._elementary_count('010', '0')).to.equal(0)
+        })
+        it('complains if asked about a non-leaf context', () => {
+            let tree = new ctw.Tree(3)
+            expect(() => {
+                tree._elementary_count('00000', '0')
+            }).to.throw(AssertionError)
+        })
+    })
+
+    describe('count', () => {
+        it('works recursively', () => {
+            let tree = new ctw.Tree(3)
+            tree = tree.increment('100', '1')
+            tree = tree.increment('000', '1')
+            expect(tree.count('00', '1')).to.equal(2)
+        })
+    })
+
+    describe('children', () => {
+        it('expands context strings by prepending them with 0 and 1', () => {
+            const tree = new ctw.Tree(5)
+            expect(tree.children('1001')).to.deep.equal(['01001', '11001'])
+        })
+
+        it('complains if the children would be too long', () => {
+            const tree = new ctw.Tree(4)
+            expect(() => {
+                tree.children('1001')
+            }).to.throw(AssertionError)
+        })
     })
 })
 
@@ -40,28 +78,6 @@ describe('scan', () => {
             ['bcd', 'e'],
             ['cde', 'f']
         ])
-    })
-})
-
-describe('_elementary_count', () => {
-    it('returns zeroes for missing contexts', () => {
-        const tree = new ctw.Tree(3)
-        expect(tree._elementary_count('010', '0')).to.equal(0)
-    })
-    it('complains if asked about a non-leaf context', () => {
-        let tree = new ctw.Tree(3)
-        expect(() => {
-            tree._elementary_count('00000', '0')
-        }).to.throw(AssertionError)
-    })
-})
-
-describe('count', () => {
-    it('works recursively', () => {
-        let tree = new ctw.Tree(3)
-        tree = tree.increment('100', '1')
-        tree = tree.increment('000', '1')
-        expect(tree.count('00', '1')).to.equal(2)
     })
 })
 
@@ -80,30 +96,16 @@ describe('compile_tree', () => {
     })
 })
 
-describe('children', () => {
-    it('expands context strings by prepending them with 0 and 1', () => {
-        const tree = new ctw.Tree(5)
-        expect(tree.children('1001')).to.deep.equal(['01001', '11001'])
-    })
-
-    it('complains if the children would be too long', () => {
-        const tree = new ctw.Tree(4)
-        expect(() => {
-            tree.children('1001')
-        }).to.throw(AssertionError)
-    })
-})
-
 describe('node_p', () => {
     it('computes correctly for leaves', () => {
         let tree = ctw.compile_tree('000' + '111', 3)
         expect(ctw.node_p(tree, '000')).to.equal(1/2)
     })
-    it('matches a really tiny example', () => {
+    it('works on the minimal example that requires recursion', () => {
         let tree = ctw.compile_tree('0' + '0', 1)
         expect(ctw.node_p(tree, '')).to.equal(1/2)
     })
-    it('matches hand calculation', () => {
+    it('matches a long-winded hand calculation', () => {
         let tree = ctw.compile_tree('00' + '110', 2)
         // Contexts and observations are the following: 00>1, 01>1, 11>0
         // So let's look at the leaf nodes and compute their weighted p.:
@@ -121,7 +123,7 @@ describe('node_p', () => {
         //       = 1/2 * 1/16 + 1/2 * 1/2 * 3/16
         expect(ctw.node_p(tree, '')).to.equal(5/64)
     })
-    it('matches the example from the paper', () => {
+    it('matches an example from the paper', () => {
         let sample = '010' + '0110100'
         let tree = ctw.compile_tree(sample, 3)
         expect(ctw.node_p(tree, '0')).to.equal(11/256)
